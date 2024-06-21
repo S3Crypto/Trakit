8b2330c70f887e496787e36613aeb635e1524e15
Mike <mike.s3crypto@gmail.com>
2024-06-21 01:04:09 +0100

    Apply Message Migration to db

--- Trakit/Migrations/20240621000259_EditMessageEntity.Designer.cs
+++ Trakit/Migrations/20240621000259_EditMessageEntity.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Trakit.Data;
 
@@ -11,9 +12,11 @@ using Trakit.Data;
 namespace Trakit.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240621000259_EditMessageEntity")]
+    partial class EditMessageEntity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Trakit/Migrations/20240621000259_EditMessageEntity.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Trakit.Migrations
+{
+    /// <inheritdoc />
+    public partial class EditMessageEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Content",
+                table: "Messages",
+                newName: "MessageContent");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "MessageContent",
+                table: "Messages",
+                newName: "Content");
+        }
+    }
+}
